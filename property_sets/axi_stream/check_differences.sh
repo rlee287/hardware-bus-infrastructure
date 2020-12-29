870ac0b5fed56585f0bb1c0baf8b65479655138c
rlee287 <rlee287@yahoo.com>
2020-12-29 10:23:44 -0800

    Print out the diff output if the check_differences script fails

--- property_sets/axi_stream/check_differences.sh
+++ property_sets/axi_stream/check_differences.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-diff axi_stream_master_monitor.v axi_stream_slave_monitor.v | diff - axis_m_to_s_diff.diff > /dev/null
+diff_output=$(diff axi_stream_master_monitor.v axi_stream_slave_monitor.v)
+echo "$diff_output" | diff - axis_m_to_s_diff.diff > /dev/null
 
 # Use true and false to avoid exiting the shell if this script is sourced
 if [ $? -eq 0 ]; then
@@ -7,5 +8,6 @@ if [ $? -eq 0 ]; then
     true
 else
     echo "AXI-Stream property pairs differ"
+    echo "$diff_output"
     false
 fi
