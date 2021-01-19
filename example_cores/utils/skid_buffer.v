d6f3fd790bdaa14e432404819bd9f9eb9ab71b2b
rlee287 <rlee287@yahoo.com>
2021-01-19 10:13:28 -0800

    Initial skid buffer
    Should be correct, but formal properties are not complete yet

---
+++ example_cores/utils/skid_buffer.v
@@ -0,0 +1,59 @@
+`default_nettype none
+
+/*
+ * A skid buffer for buffering data in valid/ready handshakes
+ * Useful for writing high-throughput AXI/AXI-Lite/AXI-Stream cores
+ */
+module skid_buffer #(
+    parameter DATA_WIDTH = 32
+) (
+    input wire clk,
+    //input wire resetn, // needed?
+
+    input wire [DATA_WIDTH-1:0] in_data,
+    input wire in_valid,
+    output reg in_ready = 1'b0,
+
+    output reg [DATA_WIDTH-1:0] out_data,
+    output reg out_valid = 1'b0,
+    input wire out_ready
+);
+
+    reg [DATA_WIDTH-1:0] data_buffer;
+    reg in_valid_delayed = 1'b0;
+    reg use_data_in_buffer = 1'b0;
+
+    always @(posedge clk)
+    begin
+        if (in_valid)
+        begin
+            data_buffer <= in_data;
+        end
+        in_valid_delayed <= in_ready;
+        // Always delay in_ready from out_ready by a clock cycle
+        in_ready <= out_ready;
+    end
+
+    always @(posedge clk)
+    begin
+        if (in_ready && out_ready)
+            use_data_in_buffer <= 1'b0;
+        else if (in_ready && !out_ready)
+            use_data_in_buffer <= 1'b1;
+    end
+
+    // Mux the data and valid signal
+    always @(*)
+    begin
+        if (use_data_in_buffer)
+        begin
+            out_valid = in_valid_delayed;
+            out_data = data_buffer;
+        end
+        else
+        begin
+            out_valid = in_valid;
+            out_data = in_data;
+        end
+    end
+endmodule
---
+++ example_cores/utils/skid_buffer_tb_formal.v
@@ -0,0 +1,105 @@
+module skid_buffer_tb_formal (
+    input wire clk,
+    //input wire resetn, // needed?
+
+    input wire [7:0] in_data,
+    input wire in_valid,
+    output reg in_ready,
+
+    output reg [7:0] out_data,
+    output reg out_valid,
+    input wire out_ready
+);
+    // We use a smaller width to speed proofs up
+    skid_buffer #(
+        .DATA_WIDTH(8)
+    ) DUT (
+        .clk(clk),
+        //.resetn(resetn)
+        .in_data(in_data),
+        .in_valid(in_valid),
+        .in_ready(in_ready),
+        .out_data(out_data),
+        .out_valid(out_valid),
+        .out_ready(out_ready)
+    );
+
+    reg past_valid = 1'b0;
+    always @(posedge clk)
+        past_valid <= 1'b1;
+
+    // psl assert !out_valid until in_valid;
+    reg in_ever_valid = 1'b0;
+    reg out_ever_valid = 1'b0;
+    always @(posedge clk)
+    begin
+        if (in_valid)
+            in_ever_valid <= 1'b1;
+        if (out_valid)
+            out_ever_valid <= 1'b1;
+    end
+    always @(posedge clk)
+    begin
+        if (past_valid && !in_ever_valid)
+            assert(!out_ever_valid);
+    end
+
+    // If both are ready, assert passthrough
+    // TODO: fails with always @(*), investigate?
+    always @(posedge clk)
+        if (in_ready && out_ready)
+        begin
+            assert(in_valid == out_valid);
+            assert(in_data == out_data);
+        end
+    
+    // Assert that in_ready is the delayed version of out_ready
+    always @(posedge clk)
+        if (past_valid)
+            assert(in_ready == $past(out_ready));
+    
+    reg [7:0] input_reg;
+
+    // These are generified AXI-Stream ports, so use those properties here
+    wire resetn_const;
+    wire [0:0] tstrb_const;
+    wire tlast_const;
+    assign resetn_const = 1'b1;
+    // Set all bits to 1's
+    assign tstrb_const = ~0;
+    assign tlast_const = 1'b1;
+    
+    axi_stream_master_monitor #(
+        .byte_width(1),
+        .id_width(0),
+        .dest_width(0),
+        .user_width(0),
+        .USE_ASYNC_RESET(1'b0),
+        .MAX_DELAY_TREADY_NO_TVALID(0) // disable
+    ) axis_out_properties (
+        .aclk(clk),
+        .aresetn(resetn_const),
+        .tvalid(out_valid),
+        .tready(out_ready),
+        .tdata(out_data),
+        .tstrb(tstrb_const),
+        .tkeep(tstrb_const),
+        .tlast(tlast_const)
+    );
+    axi_stream_slave_monitor #(
+        .byte_width(1),
+        .id_width(0),
+        .dest_width(0),
+        .user_width(0),
+        .USE_ASYNC_RESET(1'b0)
+    ) axis_in_properties (
+        .aclk(clk),
+        .aresetn(resetn_const),
+        .tvalid(in_valid),
+        .tready(in_ready),
+        .tdata(in_data),
+        .tstrb(tstrb_const),
+        .tkeep(tstrb_const),
+        .tlast(tlast_const)
+    );
+endmodule
